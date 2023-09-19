e051c2e08b023a04a2831c6d420e08035ce217b0
Tarric Sookdeo <tarricsookdeo@outlook.com>
2023-09-18 23:21:01 -0400

    Get random position for each of the images

--- js/index.js
+++ js/index.js
@@ -2,16 +2,26 @@ const imageContainer = document.getElementById('image-container');
 const headshotSrc = '../img/headshot.jpg';
 const waldoSrc = '../img/waldo.jpg';
 
+const width = window.innerWidth;
+const length = window.innerHeight;
+
 const num = 25;
 
 for (let i = 0; i < num; i++) {
   const img = document.createElement('img');
   img.src = headshotSrc;
   img.className = 'headshot';
+  img.style.position = 'absolute';
+  img.style.left = `${Math.floor(Math.random() * innerWidth - 100)}px`;
+  img.style.top = `${Math.floor(Math.random() * innerHeight - 100)}px`;
   imageContainer.append(img);
 }
 
+// Waldo image
 const img = document.createElement('img');
 img.src = waldoSrc;
 img.className = 'headshot';
+img.style.position = 'absolute';
+img.style.left = `${Math.floor(Math.random() * innerWidth - 100)}px`;
+img.style.top = `${Math.floor(Math.random() * innerHeight - 100)}px`;
 imageContainer.append(img);
