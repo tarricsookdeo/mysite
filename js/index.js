7b6a68d5c8933f6ebfbcec51ed1c50cab0ee0462
Tarric Sookdeo <tarricsookdeo@outlook.com>
2023-09-18 23:23:07 -0400

    Adjust width and height offset

--- js/index.js
+++ js/index.js
@@ -5,15 +5,15 @@ const waldoSrc = '../img/waldo.jpg';
 const width = window.innerWidth;
 const length = window.innerHeight;
 
-const num = 25;
+const num = 50;
 
 for (let i = 0; i < num; i++) {
   const img = document.createElement('img');
   img.src = headshotSrc;
   img.className = 'headshot';
   img.style.position = 'absolute';
-  img.style.left = `${Math.floor(Math.random() * innerWidth - 100)}px`;
-  img.style.top = `${Math.floor(Math.random() * innerHeight - 100)}px`;
+  img.style.left = `${Math.floor(Math.random() * innerWidth - 50)}px`;
+  img.style.top = `${Math.floor(Math.random() * innerHeight - 50)}px`;
   imageContainer.append(img);
 }
 
